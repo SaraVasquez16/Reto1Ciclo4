d300b095a733ce8f9eeb6feb0f9a88f40a5397b2
saravasquez2004 <sara.vasquez.mt@correo.usa.edu.co>
2021-12-10 20:32:53 -0500

    first commit

---
+++ src/test/java/Reto1_C4/Reto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Reto1_C4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
